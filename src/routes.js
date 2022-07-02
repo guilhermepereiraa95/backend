c2278827afe0d02fdf989f0316db29b24a2f1c0d
Guilherme Pereira <guilhermepereiraa95@gmail.com>
2022-07-02 17:31:13 -0300

    Checando erro de token

--- src/routes.js
+++ src/routes.js
@@ -13,16 +13,19 @@ routes.post('/session', SessionController.create);
 routes.put('/session/:id', SessionController.update);
 routes.delete('/session/:id', SessionController.delete);
 
-routes.get('/pedidos', PedidosController.index);
+routes.get('/pedidos', 
+// Middleware.checkAuth, 
+PedidosController.index);
+
 routes.post('/pedidos', PedidosController.create);
 routes.put('/pedidos/:id', PedidosController.update);
 routes.get('/pedidos/:id', PedidosController.show);
 
 routes.get('/pedido-produto/:id', PedidoProdutoController.show);
 routes.get('/produtos', ProdutosController.index);
-routes.post('/produtos', Middleware.checkAuth, ProdutosController.create);
-routes.get('/produtos/:id', Middleware.checkAuth, ProdutosController.show);
-routes.put('/produtos/:id', Middleware.checkAuth, ProdutosController.update);
-routes.delete('/produtos/:id', Middleware.checkAuth, ProdutosController.delete);
+routes.post('/produtos',  ProdutosController.create);
+routes.get('/produtos/:id', ProdutosController.show);
+routes.put('/produtos/:id', ProdutosController.update);
+routes.delete('/produtos/:id', ProdutosController.delete);
 
 module.exports = routes;
